2e4e39b5e8dc16f6721605c6fafd9d519fa031be
sirzelwon-hash <252958151+sirzelwon-hash@users.noreply.github.com>
2026-02-10 15:22:59 +0900

    時間計測の内部計算まで追加

--- Assets/scripts/TitleManager.cs
+++ Assets/scripts/TitleManager.cs
@@ -1,24 +1,25 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; //シーンの切り替えに必要
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    public string sceneName;            // 読み込むシーン名
 
-    public string sceneName;  //読み込むシーン名
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    //シーンを読み込む
+    // シーンを読み込む
     public void Load()
     {
         SceneManager.LoadScene(sceneName);
