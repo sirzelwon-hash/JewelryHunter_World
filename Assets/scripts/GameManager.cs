f71acb4ada10e70023a71da189aad4afb84490e9
sirzelwon-hash <252958151+sirzelwon-hash@users.noreply.github.com>
2026-02-16 11:17:49 +0900

    サウンドの追加

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -15,6 +15,11 @@ public class GameManager : MonoBehaviour
     public static GameState gameState;
     public string nextSceneName;            // 次のシーン名
 
+    //サウンド関連
+    public AudioClip meGameClear; //ゲームクリアの音源
+    public AudioClip meGameOver;  //ゲームオーバーの音
+    AudioSource soundPlayer; //AudioSorce型の変数
+
     //スコア追加
     public static int totalScore;  //合計スコア
 
@@ -22,18 +27,23 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         gameState = GameState.InGame;  //ゲームスタートしたらまずステータスをゲーム中にする
+        soundPlayer = GetComponent<AudioSource>();  //AudioSorceを参照する
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        if(gameState == GameState.GameClear)
+        if (gameState == GameState.GameClear)
         {
+            soundPlayer.Stop(); //ステージ曲を止める
+            soundPlayer.PlayOneShot(meGameClear); //ゲームクリアの曲を1回だけ鳴らす
             gameState = GameState.GameEnd; //ゲームの状態を更新
         }
 
         else if (gameState == GameState.GameOver)
         {
+            soundPlayer.Stop(); //ステージ曲を止める
+            soundPlayer.PlayOneShot(meGameOver); //ゲームオーバーの曲を1回だけ鳴らす
             gameState = GameState.GameEnd; //ゲームの状態を更新
         }
     }
